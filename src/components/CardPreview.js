822089ca0c2e6bb8d3a34f4a716705ac0a9faa55
sarah <sarorra0@gmail.com>
2022-03-29 11:54:50 -0700

    Rename Card to CardPreview and extract CheckCircle component

---
+++ src/components/CardPreview.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react';
+import { Draggable } from "react-beautiful-dnd";
+import { HiArrowsExpand } from 'react-icons/hi';
+
+import {
+  updateCard,
+  deleteCard,
+} from "../context/active-board-context/actions";
+import { useActiveBoard } from "../context/active-board-context";
+
+import ItemTitle from "./ItemTitle";
+import CardDetails from "./CardDetails";
+import CheckCircle from './CheckCircle';
+import Modal from "./Modal";
+
+const CardPreview = ({ cardId, card, index, listId }) => {
+  const [openModal, setOpenModal] = useState(false);
+  const { dispatch } = useActiveBoard();
+
+  const toggleDoneHandler = (isDone) => {
+    console.log(isDone)
+    updateCard(cardId, { done: isDone })(dispatch);
+  }
+
+  const updatTitleHandler = async (title) => {
+    updateCard(cardId, title)(dispatch);
+  };
+
+  const deleteCardHandler = async () => {
+    deleteCard(cardId, listId)(dispatch);
+  };
+  const closeModalHandler = () => {
+    setOpenModal(false);
+  }
+
+  return (
+    <>
+      {openModal && (
+        <Modal
+          component={CardDetails}
+          props={{ card, onToggleDone: toggleDoneHandler }}
+          onClose={closeModalHandler}
+        />
+      )}
+      <Draggable key={cardId} draggableId={cardId} index={index}>
+        {(dragProvided) => (
+          <div
+            className="card-container py-1"
+            ref={dragProvided.innerRef}
+            {...dragProvided.draggableProps}
+            {...dragProvided.dragHandleProps}
+          >
+            <div className="card-content p-1 flex rounded-md shadow-sm bg-gray-50 items-center hover:bg-gray-100 space-x-1">
+              <CheckCircle isDone={card.done} onToggle={toggleDoneHandler} />
+              <ItemTitle
+                textStyles={card.done ? "text-gray-400 line-through" : ""}
+                initValue={card.title}
+                onUpdateTitle={updatTitleHandler}
+                onDeleteItem={deleteCardHandler}
+              />
+
+              <div className="group cursor-pointer" onClick={() => setOpenModal(true)}>
+                <HiArrowsExpand
+                  className="text-gray-400 group-hover:text-black mx-auto" />
+              </div>
+            </div>
+          </div>
+        )}
+      </Draggable>
+    </>
+  );
+};
+
+export default CardPreview;
---
+++ src/components/CheckCircle.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { HiCheckCircle } from "react-icons/hi";
+
+const CheckCircle = ({onToggle, isDone}) => {
+    const [done, setDone] = useState(isDone)
+
+    const toggleDoneHandler = () => {
+        onToggle(!isDone)
+    }
+
+    return (  
+        <span
+        className="group w-6 cursor-pointer flex items-center pt-1"
+        onClick={toggleDoneHandler}>
+        <HiCheckCircle
+            className={`${isDone
+                ? "text-green-400 group-hover:text-gray-600 text-center"
+                : "text-gray-400 group-hover:text-green-600"
+                }`}
+        />
+    </span>
+    );
+}
+ 
+export default CheckCircle;
